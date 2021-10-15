0be44b71a8a06f29aa7f6d1d3954ecb2d612e041
JulianaLeyton-99 <67442137+JulianaLeyton-99@users.noreply.github.com>
2021-10-15 09:49:07 -0500

    Add files via upload

---
+++ Controlador/InicioSesionJefe.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Inicio De Sesion Jefe</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" href="InicioJefe.css">
+</head>
+
+<body>
+
+  <section class="SesionJefe">
+    <h5>Formulario Login</h5>
+
+    <input class="controls" type= "text" name= "usuario" value ="" placeholder ="usuario">
+    <input class="controls" type= "password" name= "contrasena" value ="" placeholder ="Contrasena">
+
+    <input class="buttons" type="submit" name="" value="Ingresar">
+
+    <p><a href="#">¿Olvidaste tu contraseña?</a></p>
+
+  </section>
+  
+</body>
+
+</html>
